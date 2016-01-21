b9535dbbaa3e26fbf91173c0b34ef5fb1ad3ff8d
wch94 <hess.372@osu.edu>
2016-01-21 15:36:56 -0500

    Check it out!

---
+++ main.rb
@@ -0,0 +1,12 @@
+require_relative "card.rb"
+require_relative "deck.rb"
+require_relative "functions.rb"
+  
+card1 = Card.new("2","Rectangle","Striped","Red")
+# card1.to_s
+# card1.fileName
+
+deck = Deck.new
+deck.to_s
+# puts deck.removeAny.to_s
+
