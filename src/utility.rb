07db28690aa28806f9595c2707a3f13bd8b98fd6
bam <moore.3071@osu.edu>
2016-01-31 17:46:42 -0500

    Fixed the location of the private specifier in Utility

--- src/utility.rb
+++ src/utility.rb
@@ -40,44 +40,6 @@ class Utility
         checkSymbol?(cardOne, cardTwo, cardThree) && checkNumber?(cardOne, cardTwo, cardThree)
   end
 
-
-  # Static method checks if colors in given three cards form a valid set configuration
-  # +cardOne+:: First card that might form a set
-  # +cardTwo+:: Second card that might form a set
-  # +cardThree+:: Third card that might form a set.
-  private def self.checkColor? (one, two, three)
-    (one.color == two.color && two.color == three.color) ||
-        (one.color != two.color && one.color != three.color && two.color != three.color)
-  end
-
-  # Static method checks if fill in given three cards form a valid set configuration
-  # +cardOne+:: First card that might form a set
-  # +cardTwo+:: Second card that might form a set
-  # +cardThree+:: Third card that might form a set.
-  private  def self.checkFill? (one, two, three)
-    (one.fillType == two.fillType && two.fillType == three.fillType) ||
-        (one.fillType != two.fillType && one.fillType != three.fillType && two.fillType != three.fillType)
-  end
-
-  # Static method checks if symbol in given three cards form a valid set configuration
-  # +cardOne+:: First card that might form a set
-  # +cardTwo+:: Second card that might form a set
-  # +cardThree+:: Third card that might form a set.
-  private def self.checkSymbol? (one, two, three)
-    (one.shapeType == two.shapeType && two.shapeType == three.shapeType) ||
-        (one.shapeType != two.shapeType && one.shapeType != three.shapeType && two.shapeType != three.shapeType)
-  end
-
-  # Static method checks if number of symbols in given three cards form a valid set configuration
-  # +cardOne+:: First card that might form a set
-  # +cardTwo+:: Second card that might form a set
-  # +cardThree+:: Third card that might form a set.
-  private def self.checkNumber? (one, two, three)
-    (one.numberOfSymbols == two.numberOfSymbols && two.numberOfSymbols == three.numberOfSymbols) ||
-        (one.numberOfSymbols != two.numberOfSymbols && one.numberOfSymbols != three.numberOfSymbols &&
-            two.numberOfSymbols != three.numberOfSymbols)
-  end
-
   # Returns true if fC, sC and tC are three different cards. False otherwise.
   # +fC+:: First card
   # +sC+:: Second card
@@ -132,4 +94,42 @@ class Utility
     end
   isValid = isValid && validRow
   isValid
+
+  private
+  # Static method checks if colors in given three cards form a valid set configuration
+  # +cardOne+:: First card that might form a set
+  # +cardTwo+:: Second card that might form a set
+  # +cardThree+:: Third card that might form a set.
+   def self.checkColor? (one, two, three)
+    (one.color == two.color && two.color == three.color) ||
+        (one.color != two.color && one.color != three.color && two.color != three.color)
+  end
+
+  # Static method checks if fill in given three cards form a valid set configuration
+  # +cardOne+:: First card that might form a set
+  # +cardTwo+:: Second card that might form a set
+  # +cardThree+:: Third card that might form a set.
+    def self.checkFill? (one, two, three)
+    (one.fillType == two.fillType && two.fillType == three.fillType) ||
+        (one.fillType != two.fillType && one.fillType != three.fillType && two.fillType != three.fillType)
+  end
+
+  # Static method checks if symbol in given three cards form a valid set configuration
+  # +cardOne+:: First card that might form a set
+  # +cardTwo+:: Second card that might form a set
+  # +cardThree+:: Third card that might form a set.
+   def self.checkSymbol? (one, two, three)
+    (one.shapeType == two.shapeType && two.shapeType == three.shapeType) ||
+        (one.shapeType != two.shapeType && one.shapeType != three.shapeType && two.shapeType != three.shapeType)
+  end
+
+  # Static method checks if number of symbols in given three cards form a valid set configuration
+  # +cardOne+:: First card that might form a set
+  # +cardTwo+:: Second card that might form a set
+  # +cardThree+:: Third card that might form a set.
+   def self.checkNumber? (one, two, three)
+    (one.numberOfSymbols == two.numberOfSymbols && two.numberOfSymbols == three.numberOfSymbols) ||
+        (one.numberOfSymbols != two.numberOfSymbols && one.numberOfSymbols != three.numberOfSymbols &&
+            two.numberOfSymbols != three.numberOfSymbols)
+  end
 end
